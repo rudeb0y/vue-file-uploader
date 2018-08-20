e6ea7735f86f7d61455c616fca0cb06b2c134059
Stephen Herron <stephen.herron23@gmail.com>
2018-08-20 20:38:42 +0100

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,32 @@
+<style>
+  form{
+    display: block;
+    height: 400px;
+    width: 400px;
+    background: #ccc;
+    margin: auto;
+    margin-top: 40px;
+    text-align: center;
+    line-height: 400px;
+      border-radius: 4px;
+  }
+</style>
+
+<template>
+  <div id="file-drag-drop">
+    <form ref="fileform">
+        <span class="drop-files">Drop the files here!</span>
+    </form>
+  </div>
+</template>
+
+<script>
+  export default {
+    data(){
+      return {
+        dragAndDropCapable: false,
+        files: []
+      }
+    }
+  }
+</script>
